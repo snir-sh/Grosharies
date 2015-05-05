c9358967b09943e20cf38c4526a397c1acdfcdf9
Ben Nakash <ben.nakash@gmail.com>
2015-05-05 12:33:40 +0300

    Basic Design

---
+++ grosharies-site/grosharies-site - Copy/web/pages/listPage.py
@@ -0,0 +1,17 @@
+#from google.appengine.api import users
+import config
+
+from google.appengine.ext.webapp import template
+
+import webapp2
+
+class IndexHandler(webapp2.RequestHandler):
+	def get(self):
+		template_params = {}			
+			
+		html = template.render("web/templates/listPage.html", template_params)
+		self.response.write(html)
+
+app = webapp2.WSGIApplication([
+	('/listPage', IndexHandler)
+], debug=True)
