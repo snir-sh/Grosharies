6794b5a9e0b279090cd9efa723dc6d09243c8ce2
Elad Mizrahi <eladmz@gmail.com>
2015-05-20 16:01:13 +0300

    Updated Facebook

--- grosharies-site/web/static/js/facebook.js
+++ grosharies-site/web/static/js/facebook.js
@@ -10,64 +10,40 @@ $(document).ready(function() {
     FB.getLoginStatus(updateStatusCallback);
   });
 });
- 
-  // This is called with the results from from FB.getLoginStatus().
-  function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
-    if (response.status === 'connected') {
-      // Logged into your app and Facebook.
-      testAPI();
-    }
-  }
-
-  // This function is called when someone finishes with the Login
-  // Button.  See the onlogin handler attached to it in the sample
-  // code below.
-  function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
-    });
-  }
-
-  // Now that we've initialized the JavaScript SDK, we call 
-  // FB.getLoginStatus().  This function gets the state of the
-  // person visiting this page and can return one of three states to
-  // the callback you provide.  They can be:
-  //
-  // 1. Logged into your app ('connected')
-  // 2. Logged into Facebook, but not your app ('not_authorized')
-  // 3. Not logged into Facebook and can't tell if they are logged into
-  //    your app or not.
-  //
-  // These three cases are handled in the callback function.
   
-  function loginFacebook() {
+function loginFacebook() {
 	FB.getLoginStatus(function(response) {
 		if (response.status === 'connected') {
-      // Logged into your app and Facebook.
-      testAPI();
-    }
-	else {
-		FB.login(function() {
-		},{'scope':'email'});
-	}
+			connected();
+		}
+		else {
+			FB.login(function(response) {
+				if (response.authResponse) {
+					connected();
+				} else {
+					console.log('User cancelled login or did not fully authorize.');
+				}
+			}, {scope: 'email'});
+		}
 	});
-  };  
+};  
 
-  // Here we run a very simple test of the Graph API after login is
-  // successful.  See statusChangeCallback() for when this call is made.
-  function testAPI() {
+function connected() {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
       console.log('Successful login for: ' + response.name + ' ' + response.email);
-	  if (document.getElementById('status') != null) {
-			document.getElementById('status').innerHTML =
-			'Thanks for logging in, ' + response.name + ' ' + response.email + '!';
+	  $.ajax({
+		url:'/login',
+		type:'GET',
+		dataType:'json',
+        data:{email:response.email},
+		success:function(data, status, xhr) {
+            location.reload();
+		},
+		error:function(xhr, status, error) {
+            alert(xhr.responseText);
+			console.error(xhr, status, error);
 		}
+	});
     });
-  }
+}
