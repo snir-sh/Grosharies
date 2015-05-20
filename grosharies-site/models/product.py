e25a168ad15c1652685e3e184f32a098105232d0
shmilove89@gmail.com <shmilove89@gmail.com>
2015-05-20 18:21:00 +0300

    UpDate product model

--- grosharies-site/models/product.py
+++ grosharies-site/models/product.py
@@ -1,7 +1,7 @@
 #this model represents a Product in our system
 
 from google.appengine.ext import ndb
-
+from models.listOfProducts import ListOfProducts
 class Product(ndb.Model):
 	ProductName = ndb.StringProperty(required =True)
 	ProductUnits = ndb.StringProperty()
@@ -20,8 +20,20 @@ class Product(ndb.Model):
 	def deleteProduct(self,product_id):
 		query = Product.query(Product.ProductID==product_id).get()
 		query.key.delete()
-		
 	
-		
+	#gets listID productName ProductUnits ProductQuantity and add it
+	#	to listOfProducts table and product table
+	@classmethod
+	def addProduct(self,product_name,product_quantity,product_units,list_id):
+		product = Product()
+		product.ProductName = product_name
+		product.ProductUnits =product_units
+		product.ProductQuantity = product_quantity
+		product.put()
+		product.ProductID =product.key.id() 
+		listOfProducts = ListOfProducts()
+		listOfProducts.ListID = list_id
+		listOfProducts.ProductID =product.key.id()
+		listOfProducts.put()
 
 		
