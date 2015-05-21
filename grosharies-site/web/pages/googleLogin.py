1478d2c239f32bdca2489fccb148adbb7ec70044
snir-sh <sshilderman@gmail.com>
2015-05-21 15:48:37 +0300

    Google login

--- grosharies-site/web/pages/googleLogin.py
+++ grosharies-site/web/pages/googleLogin.py
@@ -1,17 +1,25 @@
-#from google.appengine.api import users
 from google.appengine.ext.webapp import template
 
 from models.user import User
+from google.appengine.api import users
+from google.appengine.ext import ndb
 import webapp2
 
 class DefaultHandler(webapp2.RequestHandler):
 	def get(self):
 		template_params = {}
+		email = ''
+		googleUser = users.get_current_user()
+		if googleUser:
+			email = googleUser.email()
+			
+		#if (auth2.isSignedIn.get()) {
+		#	var profile = auth2.currentUser.get().getBasicProfile()
+		#	email = profile.getEmail()
+		#}
 		
-		html = template.render("web/templates/default.html", template_params)
-		self.response.write(html)
+		self.response.write('email is ' + email)
 
 app = webapp2.WSGIApplication([
-	('/', DefaultHandler),
-	('/default', DefaultHandler)
+	('/googleLogin', DefaultHandler)
 ], debug=True)
