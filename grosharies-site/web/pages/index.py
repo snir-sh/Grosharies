904a97e325a79e32cf0d4a9109d7cfb59aebe905
snir-sh <sshilderman@gmail.com>
2015-05-25 21:43:20 +0300

    Tests for showing groups

--- grosharies-site/web/pages/index.py
+++ grosharies-site/web/pages/index.py
@@ -2,6 +2,7 @@ from google.appengine.ext.webapp import template
 from models.user import User
 from models.group import Group
 import webapp2
+import HTML
 
 class IndexHandler(webapp2.RequestHandler):
 	def get(self):
@@ -24,6 +25,11 @@ class IndexHandler(webapp2.RequestHandler):
 				
 		#if myGroups:
 		#	template_params['groupsUser'] = groupsNames[0]
+		
+		# This is test #
+		#table_data = [['list1'],['list2'],['list3'],]
+		#htmlcode = HTML.table(table_data)
+		#print htmlcode
 						
 		html = template.render("web/templates/index.html", template_params)
 		self.response.write(html)
