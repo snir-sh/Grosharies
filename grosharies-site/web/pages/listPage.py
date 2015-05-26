5d511a57a9743145d01884d85d25185af781522a
shmilove89@gmail.com <shmilove89@gmail.com>
2015-05-26 20:25:22 +0300

    Update listPage,py

--- grosharies-site/web/pages/listPage.py
+++ grosharies-site/web/pages/listPage.py
@@ -1,6 +1,9 @@
 from google.appengine.ext.webapp import template
 from models.user import User
+from models.group import Group
 import webapp2
+import HTML
+import json
 
 class ListPageHandler(webapp2.RequestHandler):
 	def get(self):
@@ -10,7 +13,19 @@ class ListPageHandler(webapp2.RequestHandler):
 			user = User.checkToken(self.request.cookies.get('session'))
 		if not user:
 			self.redirect('/')
+		allGroups = User.getAllUserGroups(user.email)
 		
+		groupsNames = []
+		if allGroups:
+			for group in allGroups:
+				if Group.getGroupNameByID(group.GroupID):
+					name = Group.getGroupNameByID(group.GroupID).GroupName
+					groupsNames.append(name)
+				
+		template_params['userEmail'] = user.email
+				
+		if groupsNames:
+			template_params['userGroups'] = groupsNames
 		html = template.render("web/templates/listPage.html", template_params)
 		self.response.write(html)
 
