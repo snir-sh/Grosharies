9e9bfc65b307e297b7718f3e695819d64ff96bf7
snir-sh <sshilderman@gmail.com>
2015-05-27 10:38:40 +0300

    Fix logout.py

--- grosharies-site/web/pages/logout.py
+++ grosharies-site/web/pages/logout.py
@@ -10,22 +10,9 @@ class LogoutHandler(webapp2.RequestHandler):
 	def get(self):
 		template_params = {}
 		self.response.delete_cookie('session')
-		html = template.render("web/templates/logout.html", template_params)
+			
+		html = template.render("web/templates/default.html", template_params)
 		self.response.write(html)
-		
-		allGroups = User.getAllUserGroups(user.email)
-		
-		groupsNames = []
-		if allGroups:
-			for group in allGroups:
-				if Group.getGroupNameByID(group.GroupID):
-					name = Group.getGroupNameByID(group.GroupID).GroupName
-					groupsNames.append(name)
-				
-		template_params['userEmail'] = user.email
-				
-		if groupsNames:
-			template_params['userGroups'] = groupsNames
 			
 app = webapp2.WSGIApplication([
 	('/logout', LogoutHandler)
