d282b0c0fe01981b3c61076ea2af44b4b0f5b2b0
Ben Nakash <ben.nakash@gmail.com>
2015-05-31 16:57:53 +0300

    Update listPage

--- grosharies-site/web/pages/listPage.py
+++ grosharies-site/web/pages/listPage.py
@@ -28,14 +28,31 @@ class ListPageHandler(webapp2.RequestHandler):
 			listNames = List.getAllListsName(group_id)
 			if listNames:
 				template_params['groupLists'] = listNames
+			group = Group.getGroupNameByID(group_id)
+			if group:
+				template_params['groupName'] = group.GroupName
 		template_params['userEmail'] = user.email
+		
+		if group:
+			template_params['groupAdmin'] = group.GroupAdmin
+			
+		if (group.GroupAdmin==user.email):
+			template_params['isAdmin'] = user.email
+		
+		groupUsers = Group.getAllUsersFromGroupByID(group_id)
+		if groupUsers:
+			template_params['groupUsers'] = groupUsers
 		
 		groupsNames = Group.getAllGroupsNames(user.email) 		
 		if groupsNames:
 			template_params['userGroups'] = groupsNames
 		template_params['group_id'] = group_id
+		
 		html = template.render("web/templates/listPage.html", template_params)
 		self.response.write(html)
+		
+		
+		
 app = webapp2.WSGIApplication([
 	('/listPage', ListPageHandler)
 ], debug=True)
