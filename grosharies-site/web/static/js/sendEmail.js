bfd5223cc4c5c46e444effdfe5ae72818091451b
snir-sh <sshilderman@gmail.com>
2015-06-11 13:43:14 +0300

    Update email files

--- grosharies-site/web/static/js/sendEmail.js
+++ grosharies-site/web/static/js/sendEmail.js
@@ -1,13 +1,11 @@
 function send() {
-	alert("hhhh");	
-	
 	$.ajax({
 	  type: 'POST',
 	  url: 'https://mandrillapp.com/api/1.0/messages/send.json',
 	  data: {
 		'key': 'ELifZve5ZJbKw3Sc2Pik7Q',
 		'message': {
-		  'from_email': 'contactus',
+		  'from_email': 'sshilderman@gmail.com',
 		  'to': [
 			  {
 				'email': 'sshilderman@gmail.com',
@@ -17,12 +15,14 @@ function send() {
 			],
 		  'autotext': 'true',
 		  'subject': 'Grosharies: Contact Us',
-		  'html': 'Hello, this is my message body'
+		  'html': 'Hello, this is my message body!'
 		}
 	  }
-	}).done(function(response) {
-		console.log(response); // if you're into that sorta thing
 	});
+	reply();
 }
 
+function reply() {
+	alert("reply");
+}
 	
